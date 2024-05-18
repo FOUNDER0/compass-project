fbd2fd199cec91f9ded5f1b558d9bf0f4f1a3f23
Bryar Rebwar <137176897+FOUNDER0@users.noreply.github.com>
2024-05-18 18:36:29 +0300

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,26 @@
+
+//-----------dark mode-----------//
+//
+const toggleButton = document.getElementById('mode-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+});
+
+
+
+
+/*
+
+
+//-----------alart-----------//
+
+alert("Register if you are a student")
+
+let n1 = prompt("first name");
+let n2 = prompt("enter second name");
+
+alert("thank you : "+n1+" "+n2);
+
+*/
